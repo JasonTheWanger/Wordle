254ce518cae6f16e28a0649fa4d7aae1ea90a03a
JasonTheWanger <59749543+JasonTheWanger@users.noreply.github.com>
2023-01-17 14:20:50 -0800

    Add files via upload

--- WordleMain.java
+++ WordleMain.java
@@ -8,7 +8,7 @@ public class WordleMain {
 		System.out.println("Welcome to Wordle Game");
 		System.out.println("C for correct letter at correct position\nH for correct letter at incorrect position\nI for incorrect letter");
 		while (puz.getGrid().solved()==false && puz.alive()) {
-			//System.out.println(puz.getWord());
+			System.out.println(puz.getWord());
             System.out.print("\nMake a guess: ");
             String guess = scan.nextLine();
             if(puz.isWord(guess)){
@@ -19,7 +19,7 @@ public class WordleMain {
             	System.out.println("This is not a word that I know");
             clearScreen();
         }
-		if(puz.alive()) {
+		if(puz.getGrid().solved()) {
 			System.out.println("You win");
 		}
 		else {
